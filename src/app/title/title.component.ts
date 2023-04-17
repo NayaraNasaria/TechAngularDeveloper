2e1656afd3b65aa5d86695465dee1a84c9e9fcce
Nayara Nasaria Soares <nayaranasariasoares@gmail.com>
2023-04-17 17:32:29 -0300

    Aprendendo Angular

---
+++ src/app/title/title.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  templateUrl: './title.component.html',
+  styleUrls: ['./title.component.css'],
+})
+export class TitleComponent implements OnInit, OnChanges {
+  @Input() nome: string = '';
+
+  constructor() {}
+
+  ngOnChanges(): void {
+    console.log(`OnChanges ${this.nome}`);
+  }
+
+  ngOnInit(): void {
+    this.nome = `Olá ${this.nome}`;
+  }
+}
