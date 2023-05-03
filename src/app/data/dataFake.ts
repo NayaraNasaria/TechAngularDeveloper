048a6dc9d1ed4fc0d25fee6f7d7ff3c9fc5de451
Nayara Nasaria Soares <nayaranasariasoares@gmail.com>
2023-05-02 21:27:25 -0300

    Blog em Angular Simples

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,34 @@
+export const dataFake = [
+  {
+    id: '1',
+    photo: 'aa',
+    title: 'CHATGPT: Nova Tecnologia para ajudar?',
+    description:
+      'ChatGPT é um assistente virtual inteligente no formato chatbot online.',
+    photoCover:
+      'https://nosbastidores.com.br/wp-content/uploads/2023/04/chatgpt.jpeg',
+  },
+  {
+    id: '2',
+    photo: 'aa',
+    title: 'The Last of Us: A Série inspirada no game e seu sucesso',
+    description: 'Série de TV com 8,9 de aprovação.',
+    photoCover:
+      'https://s2.glbimg.com/DAApUuQTGfAvfz6lpD5NozUzlqI=/0x0:1024x683/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_e536e40f1baf4c1a8bf1ed12d20577fd/internal_photos/bs/2023/f/L/CZEOlsQoSQddAM6OHpIA/div.jpg',
+  },
+  {
+    id: '3',
+    photo: 'aa',
+    title: 'Sucesso de filmes inspirados em games: Super Mario Bros',
+    description: 'Filme faz sucesso nas bilheterias!',
+    photoCover:
+      'https://sm.ign.com/ign_br/movie/s/super-mari/super-mario-bros-o-filme_hnx9.jpg',
+  },
+  {
+    id: '4',
+    photo: 'aa',
+    title: 'Avatar: O Caminho da Água é um espetáculo visual impressionante',
+    description: 'O filme arrecadou 2,3 bilhões.',
+    photoCover: 'https://images8.alphacoders.com/106/thumb-1920-1063148.jpg',
+  },
+];
